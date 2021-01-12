d1a540ddaca9ad386f37f50286b60134a444ad81
Marty Warfel <marthawarfel@gmail.com>
2021-01-11 22:03:30 -0600

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,16 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+    get '/name' do
+        "My name is Marty"
+    end
+
+    get '/hometown' do
+        "My hometown is Warrenville, IL"
+    end
+
+    get '/favorite-song' do
+        "My favorite song is 'Five Years' by David Bowie"
+    end
+
 end
